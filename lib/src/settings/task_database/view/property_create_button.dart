82f76020d2b1730f95cd3bc9ffa1a33f1409431f
Yumiko Kokubu <ymymyummy@gmail.com>
2024-12-10 06:21:39 +0900

    add style

--- lib/src/settings/task_database/view/property_create_button.dart
+++ lib/src/settings/task_database/view/property_create_button.dart
@@ -15,80 +15,89 @@ class PropertyCreateButton extends StatelessWidget {
   final Future<void> Function() onDatabaseRefreshed;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        final propertyName = await showDialog<String>(
-          context: context,
-          builder: (BuildContext context) {
-            String tempName = '';
-            return AlertDialog(
-              content: TextField(
-                onChanged: (value) {
-                  tempName = value;
-                },
-                decoration: const InputDecoration(
-                  hintText: 'プロパティ名',
-                ),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
+    return SizedBox(
+      height: 30,
+      child: FilledButton(
+        style: ButtonStyle(
+          padding: WidgetStateProperty.all(
+            const EdgeInsets.symmetric(vertical: 2, horizontal: 16),
+          ),
+        ),
+        onPressed: () async {
+          final propertyName = await showDialog<String>(
+            context: context,
+            builder: (BuildContext context) {
+              String tempName = '';
+              return AlertDialog(
+                content: TextField(
+                  onChanged: (value) {
+                    tempName = value;
                   },
-                  child: const Text('キャンセル'),
+                  decoration: const InputDecoration(
+                    hintText: 'プロパティ名',
+                  ),
                 ),
-                TextButton(
-                  onPressed: () async {
-                    final errorMessage = tempName.trim().isEmpty
-                        ? 'プロパティ名を入力してください'
-                        : await selectedDatabaseViewModel
-                                .checkPropertyExists(tempName)
-                            ? 'すでに同じ名前のプロパティが存在します'
-                            : null;
-                    if (errorMessage != null) {
-                      showDialog(
-                        // ignore: use_build_context_synchronously
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('エラー'),
-                            content: Text(errorMessage),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                      return;
-                    }
-                    Navigator.of(context).pop(tempName);
-                  },
-                  child: const Text('確定'),
-                ),
-              ],
-            );
-          },
-        );
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('キャンセル'),
+                  ),
+                  FilledButton(
+                    onPressed: () async {
+                      final errorMessage = tempName.trim().isEmpty
+                          ? 'プロパティ名を入力してください'
+                          : await selectedDatabaseViewModel
+                                  .checkPropertyExists(tempName)
+                              ? 'すでに同じ名前のプロパティが存在します'
+                              : null;
+                      if (errorMessage != null) {
+                        showDialog(
+                          // ignore: use_build_context_synchronously
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('エラー'),
+                              content: Text(errorMessage),
+                              actions: [
+                                FilledButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                        return;
+                      }
+                      // ignore: use_build_context_synchronously
+                      Navigator.of(context).pop(tempName);
+                    },
+                    child: const Text('確定'),
+                  ),
+                ],
+              );
+            },
+          );
 
-        if (propertyName != null) {
-          final property = await selectedDatabaseViewModel.createProperty(
-              type, propertyName);
+          if (propertyName != null) {
+            final property = await selectedDatabaseViewModel.createProperty(
+                type, propertyName);
 
-          // データベースが再取得されるまで待つ
-          await onDatabaseRefreshed();
+            // データベースが再取得されるまで待つ
+            await onDatabaseRefreshed();
 
-          final propertyType = switch (type) {
-            CreatePropertyType.date => SettingPropertyType.date,
-            CreatePropertyType.checkbox => SettingPropertyType.status
-          };
+            final propertyType = switch (type) {
+              CreatePropertyType.date => SettingPropertyType.date,
+              CreatePropertyType.checkbox => SettingPropertyType.status
+            };
 
-          selectedDatabaseViewModel.selectProperty(property.id, propertyType);
-        }
-      },
-      child: const Text('新規作成'),
+            selectedDatabaseViewModel.selectProperty(property.id, propertyType);
+          }
+        },
+        child: const Text('新規作成', style: TextStyle(fontSize: 12)),
+      ),
     );
   }
 }
