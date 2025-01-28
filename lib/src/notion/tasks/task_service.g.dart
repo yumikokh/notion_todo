078e71ef99af372dd4819fc4f1654ed91c567e60
Yumiko Kokubu <ymymyummy@gmail.com>
2025-01-29 08:40:16 +0900

    refactor: Optimize TaskService and TaskViewModel with Riverpod provider
    - Add Riverpod provider for TaskService initialization
    - Update TaskViewModel to use the new taskServiceProvider
    - Modify task-related methods to handle nullable TaskService
    - Generate updated Riverpod code for TaskViewModel

---
+++ lib/src/notion/tasks/task_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$taskServiceHash() => r'3dbc811805cffd9bc08624eb834b34bcf476354c';
+
+/// See also [taskService].
+@ProviderFor(taskService)
+final taskServiceProvider = AutoDisposeFutureProvider<TaskService?>.internal(
+  taskService,
+  name: r'taskServiceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$taskServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef TaskServiceRef = AutoDisposeFutureProviderRef<TaskService?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
