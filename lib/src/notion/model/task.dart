af63424cc0e19976b006a56dd471ffa321e622d7
Yumiko Kokubu <ymymyummy@gmail.com>
2025-01-30 01:35:43 +0900

    refactor: Enhance Task model with flexible status representation
    - Introduce TaskStatus sealed class to support checkbox and status-based task completion
    - Add methods to determine task completion and in-progress states
    - Update Task model to use TaskStatus instead of simple boolean
    - Modify TaskService to handle different status property types
    - Generate necessary freezed and JSON serialization code

--- lib/src/notion/model/task.dart
+++ lib/src/notion/model/task.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:tanzaku_todo/src/notion/model/property.dart';
 
 part 'task.freezed.dart';
 part 'task.g.dart';
@@ -15,19 +16,45 @@ class TaskDate with _$TaskDate {
 }
 
 @freezed
+sealed class TaskStatus with _$TaskStatus {
+  const factory TaskStatus.checkbox({
+    required bool checked,
+  }) = TaskStatusCheckbox;
+
+  const factory TaskStatus.status({
+    required StatusGroup? group,
+    required StatusOption? option,
+  }) = TaskStatusStatus;
+
+  factory TaskStatus.fromJson(Map<String, dynamic> json) =>
+      _$TaskStatusFromJson(json);
+}
+
+@freezed
 class Task with _$Task {
+  const Task._();
+
   const factory Task({
     required String id,
     required String title,
-    required bool isCompleted,
+    required TaskStatus status,
     required TaskDate? dueDate,
     required String? url,
     // required String createdTime,
     // required String updatedTime,
   }) = _Task;
 
-  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+  bool get isCompleted => switch (status) {
+        TaskStatusCheckbox(checked: var checked) => checked,
+        TaskStatusStatus(group: var group) =>
+          group?.name == StatusGroupType.complete.value,
+      };
 
-  factory Task.initial() => const Task(
-      id: '', title: '', isCompleted: false, dueDate: null, url: null);
+  bool get isInProgress => switch (status) {
+        TaskStatusCheckbox() => false,
+        TaskStatusStatus(group: var group) =>
+          group?.name == StatusGroupType.inProgress.value,
+      };
+
+  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
 }
--- lib/src/notion/model/task.g.dart
+++ lib/src/notion/model/task.g.dart
@@ -18,10 +18,44 @@ Map<String, dynamic> _$$TaskDateImplToJson(_$TaskDateImpl instance) =>
       'end': instance.end,
     };
 
+_$TaskStatusCheckboxImpl _$$TaskStatusCheckboxImplFromJson(
+        Map<String, dynamic> json) =>
+    _$TaskStatusCheckboxImpl(
+      checked: json['checked'] as bool,
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$TaskStatusCheckboxImplToJson(
+        _$TaskStatusCheckboxImpl instance) =>
+    <String, dynamic>{
+      'checked': instance.checked,
+      'runtimeType': instance.$type,
+    };
+
+_$TaskStatusStatusImpl _$$TaskStatusStatusImplFromJson(
+        Map<String, dynamic> json) =>
+    _$TaskStatusStatusImpl(
+      group: json['group'] == null
+          ? null
+          : StatusGroup.fromJson(json['group'] as Map<String, dynamic>),
+      option: json['option'] == null
+          ? null
+          : StatusOption.fromJson(json['option'] as Map<String, dynamic>),
+      $type: json['runtimeType'] as String?,
+    );
+
+Map<String, dynamic> _$$TaskStatusStatusImplToJson(
+        _$TaskStatusStatusImpl instance) =>
+    <String, dynamic>{
+      'group': instance.group,
+      'option': instance.option,
+      'runtimeType': instance.$type,
+    };
+
 _$TaskImpl _$$TaskImplFromJson(Map<String, dynamic> json) => _$TaskImpl(
       id: json['id'] as String,
       title: json['title'] as String,
-      isCompleted: json['isCompleted'] as bool,
+      status: TaskStatus.fromJson(json['status'] as Map<String, dynamic>),
       dueDate: json['dueDate'] == null
           ? null
           : TaskDate.fromJson(json['dueDate'] as Map<String, dynamic>),
@@ -32,7 +66,7 @@ Map<String, dynamic> _$$TaskImplToJson(_$TaskImpl instance) =>
     <String, dynamic>{
       'id': instance.id,
       'title': instance.title,
-      'isCompleted': instance.isCompleted,
+      'status': instance.status,
       'dueDate': instance.dueDate,
       'url': instance.url,
     };
