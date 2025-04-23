69423ba6403e7769869dc97a913f10760ecad6bf
Yumiko Kokubu <ymymyummy@gmail.com>
2025-04-24 03:30:38 +0900

    fix: Adjust date handling in DateSheetViewModel for local time representation
    - Updated the focusedDay getter to return the local time of the focused day.
    - Modified the calenderFirstDay method to ensure the initial start date is also returned in local time, enhancing date accuracy.

--- lib/src/notion/tasks/date_sheet_viewmodel.dart
+++ lib/src/notion/tasks/date_sheet_viewmodel.dart
@@ -22,7 +22,7 @@ class DateSheetViewModel extends ChangeNotifier {
       _selectedDateTime?.end?.datetime.toLocal();
 
   DateTime _focusedDay;
-  DateTime get focusedDay => _focusedDay;
+  DateTime get focusedDay => _focusedDay.toLocal();
 
   DateTime get calenderFirstDay {
     final initial = initialDateTime;
@@ -30,7 +30,9 @@ class DateSheetViewModel extends ChangeNotifier {
     if (initial == null) {
       return now;
     }
-    return initial.start.datetime.isBefore(now) ? initial.start.datetime : now;
+    return initial.start.datetime.isBefore(now)
+        ? initial.start.datetime.toLocal()
+        : now;
   }
 
   DateTime get calenderLastDay {
